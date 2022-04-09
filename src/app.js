968127e2d860bb62d593d53d17384ab5939e598e
msgimanila <msgimanila@yahoo.com>
2022-04-09 17:59:36 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,3 +10,6 @@ const person = {
 
 // Display data from the object:
 document.getElementById("demo").innerHTML = person.fullName();
+info="The <a href=&#34;#&#34;>Link</a><h1>Philippines</h1>, officially the Republic of the Philippines, is an archipelagic country in Southeast Asia. It is situated in the western Pacific Ocean, and consists of about 7,640 islands, that are broadly categorized under three main geographical divisions from north to south: Luzon, Visayas, and Mindanao.<button></button>"
+
+document.write(info)
