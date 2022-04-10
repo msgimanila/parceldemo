561a4630de2b2899796d02fb3b5f51bfc4726bbc
msgimanila <msgimanila@yahoo.com>
2022-04-10 12:15:15 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import answer from './modules/module1.js';
 import getTheAnswer from './modules/module2.js';
 var name = 'Radagast';
-
+var answer = 'thisisthe answer';
 console.log(answer);
 
 alert('The answer is ' + getTheAnswer());
