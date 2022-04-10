7405d648cd72c9037d33df14e030933a744e01dd
msgimanila <msgimanila@yahoo.com>
2022-04-10 11:52:59 +0800

    Update style.js

--- src/style.js
+++ src/style.js
@@ -1,15 +1,10 @@
 export function hi (name) {
     return `Hello, ${name}, You are using a module `;
     }
-export let x = function js_thml(){
-	let code = "";
-	code += '<h1>HTML To JavaScript</h1>\n';
-	code += '<ol>\n';
-	code += '\t<li>First item</li>\n';
-	code += '\t<li>Second item</li>\n';
-	code += '\t<li>Third item</li>\n';
-	code += '\t<li>Fourth item</li>\n';
-	code += '</ol>\n';
-	return code;
-}
-document.getElementById("js_html").innerHTML = x;
+var answer = 42;
+
+var getTheAnswer = function () {
+	return answer;
+};
+
+export getTheAnswer;
