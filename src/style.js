988802e1fe178cf091b0e59236d32740459a1151
msgimanila <msgimanila@yahoo.com>
2022-04-10 11:54:54 +0800

    Update style.js

--- src/style.js
+++ src/style.js
@@ -1,7 +1,8 @@
 export function hi (name) {
     return `Hello, ${name}, You are using a module `;
     };
-var answer = 42;
+
 export getTheAnswer = function () {
+	var answer = 42;
 	return answer;
 }; 
